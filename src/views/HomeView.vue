11d9a10d966892523e6cfedaee1dd23230b086fc
AchiUgulava <achiugulava@gmail.com>
2023-11-11 18:36:45 +0400

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup>
+// import { useRouter } from 'vue-router'
+// import { useDarkModeStore } from '@/stores/darkMode.js'
+// import { gradientBgPurplePink } from '@/colors.js'
+// import SectionMain from '@/components/SectionMain.vue'
+// import CardBox from '@/components/CardBox.vue'
+import LayoutGuest from '@/layouts/LayoutGuest.vue'
+
+// const styles = ['white', 'basic']
+
+// const darkModeStore = useDarkModeStore()
+
+// darkModeStore.set(false)
+
+// const router = useRouter()
+
+// const handleStyleChange = (slug) => {
+//   document.documentElement.classList.forEach((token) => {
+//     if (token.indexOf('style') === 0) {
+//       document.documentElement.classList.replace(token, `style-${slug}`)
+//     }
+//   })
+
+//   router.push('/dashboard')
+// }
+</script>
+
+<template>
+  <LayoutGuest>
+    hello
+  </LayoutGuest>
+</template>
