d0b8f681fef1bee2f24658ed4540dcf57be5f954
gia-grigalashvili <grigalagia07@gmail.com>
2023-11-12 02:32:38 +0400

    finaliii

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,6 +27,905 @@ import LayoutGuest from '@/layouts/LayoutGuest.vue'
 
 <template>
   <LayoutGuest>
-    hello
+    <div class="small-nav">
+        <h4 class="small-text">MindSetter</h4>
+    </div>
+
+    <div class="nav">
+        <div class="nav-child">
+            <img src="asxax.png" class="nasa-logo">
+            <a href="#" class="link-a"><h1>Home</h1><i class="fa-solid fa-chevron-down"></i></a>
+            <a href="sound.html" class="link-a"><h1>Software</h1><i class="fa-solid fa-chevron-down"></i></a>
+        </div>
+    </div>
+
+
+    <div class="nasa" style="display: flex;">
+        <div class="back-blur">
+          <div class="back-half">
+            <div class="background-abouts">
+                <h1>Education</h1>
+                <h2>Enjoy a cinema experience in you own home with exlusive premieres & hundreds of movies to rent or buy on your PlayStation console</h2>
+                <div class="back-button"><h3>Find out more</h3></div>
+            </div>
+        </div>
+      </div> 
+    </div>
+
+    <div class="moon" style="display: none;">
+        <div class="back-blur">
+          <div class="back-half">
+            <div class="background-abouts">
+                <h1>Sound in MindSetter</h1>
+                <h2>Enjoy a cinema experience in you own home with exlusive premieres & hundreds of movies to rent or buy on your PlayStation console</h2>
+                <div class="back-button"><h3>Find out more</h3></div>
+            </div>
+        </div>
+      </div> 
+    </div> 
+
+
+    <div class="sspace" style="display: none;">
+        <div class="back-blur">
+          <div class="back-half">
+            <div class="background-abouts">
+                <h1>2D astronomical data</h1>
+                <h2>Enjoy a cinema experience in you own home with exlusive premieres & hundreds of movies to rent or buy on your PlayStation console</h2>
+                <div class="back-button"><h3>Find out more</h3></div>
+            </div>
+        </div>
+      </div> 
+    </div> 
+
+
+    <div class="three-image">
+        <div class="three-image-child"><img src="foto2.png"> <div class="black-blur1" onclick="nasaActive()"></div></div>
+        <div class="three-image-child"><img src="image12.png"> <div class="black-blur2" onclick="moonActive()"></div></div>
+        <div class="three-image-child"><img src="afrika.png">  <div class="black-blur3" onclick="sspaceActive()"></div></div>
+    </div>
+    
+
+    <div class="SoundsOfSpacehistori">
+        <div class="text">
+            <div class="h1text">
+                <h1 class="text2">MindSetter </h1>
+            </div>
+        
+        <p>
+           
+            In the dynamic realm of digital education,
+             MindSetter is at the forefront, revolutionizing student experiences by adeptly
+              addressing challenges spanning organizational capabilities to well-being. With
+               a keen focus on enhancing learning through features such as organizational prowess,
+                well-being in digital education, data-driven educational strategies, and an overall
+                 commitment to an enriched learning experience,
+             MindSetter stands as a transformative force in the evolving landscape of digital learning.
+        </p>
+
+       <div class="button-div">
+<button  type="submit" >
+    learn more
+</button>
+
+</div>
+        </div>
+        <div class="SoundsOfSpacehistori-img"> 
+       
+        </div>
+     
+    
+    </div>
+
+   
+
+    <div class="main-video">
+
+<div class="videop">
+
+
+    <img class="video" src="image16.png" alt="">
+    <div class="video-text">
+        <h1>
+            Research Alignment
+        </h1>
+        <p> MindSetter is inspired by the groundbreaking research article, 
+    “The Impact of Organizational Stress on Academic Performance: 
+    A Systematic Review.” This study delves into the challenges students face,
+     emphasizing the negative effects of stress on academic achievements. 
+     Our platform is designed to align seamlessly with these insights, providing
+      practical and enthusiastic solutions to alleviate these burdens</p>
+
+
+             
+
+    </div>
+   
+</div>
+
+
+
+    </div>
+
+
+
+
+
+ <div class="SoundsOfSpace">
+
+      
+
+
+    <div class="text-SoundsOfSpace">
+        <div class="text-123">
+            <h1 class="text-SoundsOfSpace2">Impact</h1>
+<p>
+    
+    Short-term: Immediate relief from organizational burdens and stress, creating a more focused and engaged learning experience.
+
+Long-term: Sustainable practices that not only benefit individual students but contribute to a broader environmental agenda by reducing waste and promoting eco-friendly alternatives.
+
+Welcome to the future of education—Welcome to MindSetter, where continuous development meets future competencies to shape a smarter and more sustainable academic journey.
+</p>
+
+        </div>
+
+
+    </div>
+
+      <div class="SoundsOfSpace-img">
+            <img class="SoundsOfSpace-img" src="images/sound4.jpg" alt="">
+        </div>
+
+    </div>
+
+
+
+    <div class="join-now">
+        <div class="join-now-btn">
+            <button class="join-now-btn-h1"><h1>join-now</h1></button>
+        </div>
+       
+    </div>
+
+
+
+
+
+
+
+
+<footer>
+
+    <div class="footer-main">
+        <div class="img-footer">
+            <img src="asxax.png" alt="">
+        </div>
+    
+        <div class="footer-txt-1">
+    
+    <h1>The National Aeronautics <br> and
+                              MindSetter</h1>
+    <h2>MindSetter explores the unknown in air and MindSetter, <br> innovates for the benefit of humanity, and inspires the <br> world through discovery.</h2>
+    <h3>About MindSetter's Mission</h3>
+    <h4>Join Us</h4>
+    <img src="./Screenshot 2023-10-07 at 15.57.25-fotor-bg-remover-2023100721016.png" alt="">
+
+
+
+
+    
+    
+        </div>
+    <div class="footer-sections">
+        <h1>Home <br> <br>
+            News & Events <br> <br>
+            Multimedia <br> <br>
+            MindSetter TV <br> <br>
+            Missions
+            </h1>
+    </div>
+    
+    <div class="footer-sections-1">
+        <h1>
+    Humans in MindSetter <br> <br>
+    Earth & Climate <br> <br>
+    The Solar System <br> <br>
+    The Universe <br> <br> 
+    Science
+            </h1>
+    </div>
+    
+    <div class="footer-sections-2">
+        <h1>
+    Aeronautics <br><br>
+    Technology <br><br>
+    Learning Resources <br><br>
+    About MindSetter <br><br>
+    MindSetter en Español
+            </h1>
+    </div>
+    
+    
+    <div class="footer-txt-2">
+    
+        <h1>Follow MindSetter</h1>
+        <img src="./Screenshot 2023-10-07 at 16.05.42-fotor-bg-remover-2023100721332.png" alt="">
+    
+    </div>
+    
+    
+    <div class="footer-sections-3">
+        <h2>More MindSetter Social Accounts <br><br>
+            MindSetter Newsletters</h2>
+    </div>
+    
+    <div class="line"></div>
+    
+    
+    </div>
+    
+        </footer>
+
+
+
+   
+  
+
+
+
+
+
+   
   </LayoutGuest>
 </template>
+
+<style scoped>
+
+
+*{
+    margin: 0;
+    padding: 0;
+    caret-color: #00000000;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap');
+
+.small-nav{
+    width: 100%;
+    height: 30px;
+
+    background-color: #ffff;
+
+    display: flex;
+    align-items: center;
+    justify-content: end;
+}
+.small-text{
+    font-family: 'Caveat', cursive;
+    margin-right: 20px;
+
+    color: black;
+}
+
+.nav{
+    width: 100%;
+    height: 65px;
+
+    background-color: rgb(13, 13, 25);
+}
+
+.nasa-logo{
+    width: 63px;
+    height: 63px;
+
+    margin-left: 22px;
+}
+
+.nav-child{
+    width: 100%;
+    height: 68px;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+}
+
+.link-a{
+    margin-left: 35px;
+    text-decoration: none;
+    font-family: 'Inter', sans-serif;
+    font-size:  8px;
+    letter-spacing: 2px;
+
+    display: flex;
+}
+.link-a i{
+    font-size: 12px;
+    color: rgb(255, 255, 255);
+
+    margin-left: 4px;
+    margin-top: 3px;
+}
+.link-a h1{
+    color: rgb(255, 255, 255);
+    font-size: 20px;
+    font-weight: 40px;
+}
+
+
+.nasa{
+    background-size: 100%, 25%, 25%;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 550px;
+    background-image: url(Technology.jpg);
+    margin-top: 1px;
+}
+.moon{
+    background-size: 100%, 25%, 25%;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 550px;
+    background-image: url(foto2.png);
+}
+
+.sspace{
+    background-size: 100%, 25%, 25%;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 550px;
+    background-image: url(images/sound4.jpg);
+}
+.back-half{
+    width: 50%;
+    height: 550px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.back-blur{
+    width: 100%;
+    height: 550px;
+    background-color: rgba(0, 0, 0, 0.535);
+
+    display: flex;
+    align-items: center;
+}
+.background-abouts{
+    width: 80%;
+    height: 260px;
+
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+}
+.background-abouts h1{
+    font-family: 'Caveat', cursive;
+    color: #ffff;
+    font-size: 67px;
+}
+.background-abouts h2{
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    color: #ffff;
+    font-weight: 300;
+    font-size: 16px;
+    margin-left: 6px;
+}
+.back-button{
+    width: 200px;
+    height: 42px;
+    border-radius: 6px;
+    background-color: rgb(0, 0, 0);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    cursor: pointer;
+}
+.back-button:hover{
+    width: 200px;
+    height: 42px;
+    border-radius: 6px;
+    background-color: rgba(0, 0, 0, 0.781);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    cursor: pointer;
+}
+.back-button h3{
+    color: #ffffff;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.mtvare2{
+    background-image: url(images/mtvare2.jpg);
+    margin-top: 44px;
+    
+    background-repeat: no-repeat;
+    border-radius: 20px;
+    width: 25%;
+    height: 200px;
+}
+
+.three-image{
+    width: 80%;
+    height: 180px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+}
+.three-image-child{
+    width: 215px;
+    height: 125px;
+
+    margin-left: 8px;
+    margin-right: 8px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.three-image-child img{
+    width: 214px;
+    height: 124px;
+    border-radius: 16px;
+    position: absolute;
+}
+
+.black-blur1{
+    width: 214px;
+    height: 124px;
+    border-radius: 16px;
+   
+    position: absolute;
+    cursor: pointer;
+
+    border-radius: 16px;
+    border: 2px solid rgb(0 114 206);
+    padding: 2px;
+}
+.black-blur1:hover{
+    background-color: rgba(0, 0, 0, 0.385);
+}
+
+.black-blur2{
+    width: 214px;
+    height: 124px;
+    border-radius: 16px;
+
+    position: absolute;
+    cursor: pointer;
+
+    border-radius: 16px;
+    border: 0px solid rgb(0 114 206);
+    padding: 2px;
+}
+.black-blur2:hover{
+    background-color: rgba(0, 0, 0, 0.385);
+}
+
+.black-blur3{
+    width: 214px;
+    height: 124px;
+    border-radius: 16px;
+    
+    position: absolute;
+    cursor: pointer;
+
+    border-radius: 16px;
+    border: 0px solid rgb(0 114 206);
+    padding: 2px;
+}
+.black-blur3:hover{
+    background-color: rgba(0, 0, 0, 0.385);
+}
+
+
+
+
+
+
+.text{
+    width: 45%;
+    margin:auto;
+    
+   
+}
+.text2{
+    font-weight: bolder;
+    font-size: larger;
+}
+
+.SoundsOfSpacehistori-img{
+    background-image: url(image15.png);
+    background-position: 0% 75%;
+    background-size: 100%, 5%, 25%;
+    border-radius: 10px;
+    margin-right: 53px;
+    margin-top: 42px;
+    width: 570px;
+}
+
+.SoundsOfSpacehistori{
+    display: flex;
+    justify-content: center;
+    height: 400px;
+    margin-top: 30px;
+   
+    
+        
+    }
+
+    .h1text h1{
+        font-size: 30px;
+        font-family: 'Montserrat', sans-serif;
+
+       
+    }
+    .SoundsOfSpacehistori p{
+        font-size: 20px;
+        margin-top: 4%;
+        font-family: 'Montserrat', sans-serif;
+        width: 85%;
+    }
+    
+.SoundsOfSpacehistori-img p{
+    color: rgb(255, 255, 255);
+ text-align: center;
+ margin-top: 150px;
+ margin-left: 332px;
+ font-size: 30px;
+}
+
+
+.text-SoundsOfSpace{
+    width: 50%;
+    margin:auto;
+    
+}
+.text-SoundsOfSpace2{
+    font-weight: bolder;
+    font-size: larger;
+}
+.text-123{
+    margin-left: -82px;
+}
+.text-123 h1{
+    font-size: 30px;
+    font-family: 'Montserrat', sans-serif;
+    margin-top: 90px;
+
+}
+
+.text-123 p{
+    font-size: 20px;
+    margin-top: 4%;
+    font-family: 'Montserrat', sans-serif;
+    width: 90%;
+    
+}
+
+.SoundsOfSpace{
+    display: flex;
+    justify-content: center;
+    height: 400px;
+    margin-top: 30px;
+    width: 90%;
+    margin-left: 120px;
+}
+
+
+.SoundsOfSpace-img{
+
+    border-radius: 10px;
+    margin-right: -5px;
+    margin-top: 42px;
+    width: 600px;
+    
+     background-image: url(hashu0916.png);
+    margin-left: -25px;
+}
+
+
+
+.videop{
+    display: flex;
+    justify-content: center;
+    margin-top: 80px;
+   height: 500px;
+    background-color: rgb(13, 13, 25);
+    box-shadow: rgb(132, 34, 30);
+    
+ 
+}
+.main-video{
+    background-image: url(images/erwrwrwr.jpg) ;
+   width: 100%;
+ 
+   border-radius: 2px;
+   
+   background-color: rgba(0, 0, 0, 0.535);
+   
+   
+   
+}
+
+
+
+
+
+
+
+
+
+.button-div button {
+    margin-top: 2%;
+    position: absolute;
+    width: 148px;
+    height: 45px;
+    background-color: rgba(0, 0, 0, 0);
+    color: rgb(255, 255, 255);
+    font-family: 'Gabarito', cursive;
+    font-size: 18px;
+    transition: 0.5s;
+    cursor: pointer;
+    border: 2px solid rgb(255, 255, 255);
+    border-radius: 50px;
+    margin-top: 41px;
+   
+    
+}
+
+.button-div button:hover{
+    color: white;
+    border: 2px solid blue;
+    background-color: blue;
+    transition: 0.5s;
+
+}
+
+
+.video-text{
+    width: 35%;
+    margin:auto;
+    color: white;
+    margin-top: 57px;
+    
+}
+.video{
+    border-radius: 20px; 
+    border-radius: 10px;
+    margin-right: 53px;
+    margin-top: 42px;
+    width: 471px;
+    margin-left: 92px;
+    height: 399px;
+}
+
+.video-text h1{
+    font-size: 30px;
+    font-family: 'Montserrat', sans-serif;
+    
+
+}
+
+
+.video-text p{
+    font-size: 20px;
+    margin-top: 5%;
+    font-family: 'Montserrat', sans-serif;
+    width: 90%;
+}
+
+
+.join-now{
+
+    width: 100%;
+    height: 50px;
+
+}
+
+.join-now-btn{
+width: 200px;
+height: 60px;
+background-color: rgb(26, 27, 55);
+
+margin-left: 650px;
+    margin-top: 50px;
+    border-radius: 10px;
+    text-align: center;
+   
+}
+
+.join-now-btn-h1  {
+  margin-top: 13px;
+color: white;
+font-size: 20px;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+.footer-main{
+    margin-top: 3%;
+    background-color: black;
+    width: 100%;
+    height: 400px;
+   
+    background-size: 50%;
+}
+
+.img-footer img{
+    position: relative;
+    height: 70px;
+    margin-top: 50px;
+    margin-left: 3%;
+}
+
+.footer-txt-1{
+    position: absolute;
+    font-family: 'Gabarito', cursive;
+    color: white;
+    margin-left: 3%;
+
+    cursor: pointer;
+}
+
+.footer-txt-1 h1{
+    font-size:  25px;
+    margin-top: 3%;
+
+    cursor: pointer;
+}
+
+.footer-txt-1 h2{
+    font-size:  15px;
+    margin-top: 6%;
+    font-family: 'Open Sans', sans-serif;
+    color: rgb(177, 177, 177);
+
+    cursor: pointer;
+}
+
+.footer-txt-1 h3{
+    font-size:  15px;
+    margin-top: 6%;
+    font-family: 'Open Sans', sans-serif;
+    color: rgb(255, 255, 255);
+
+    cursor: pointer;
+}
+
+.footer-txt-1 h4{
+    font-size:  20px;
+    margin-top: 3%;
+    position: absolute;
+
+    cursor: pointer;
+}
+
+.footer-txt-1 img{
+    position: absolute;
+    height: 35px;
+    margin-left: 18%;
+    margin-top: 1.5%;
+}
+
+
+.footer-sections{
+    position: absolute;
+    margin-top: 0%;
+    margin-left: 40%;
+    font-family: 'Open Sans', sans-serif;
+    color: rgb(255, 255, 255);
+}
+
+.footer-sections h1{
+    font-size: 13.5px;
+
+    cursor: pointer;
+    
+}
+
+
+.footer-sections-1{
+    position: absolute;
+    margin-top: 0%;
+    margin-left: 52.5%;
+    font-family: 'Open Sans', sans-serif;
+    color: rgb(255, 255, 255);
+}
+
+.footer-sections-1 h1{
+    font-size: 13.5px;
+
+    cursor: pointer;
+    
+}
+
+
+.footer-sections-2{
+    position: absolute;
+    margin-top: 0%;
+    margin-left: 67%;
+    font-family: 'Open Sans', sans-serif;
+    color: rgb(255, 255, 255);
+}
+
+.footer-sections-2 h1{
+    font-size: 13.5px;
+    cursor: pointer;
+    
+}
+
+
+
+.footer-txt-2{
+    position: absolute;
+    font-family: 'Gabarito', cursive;
+    color: rgb(146, 146, 146);
+    margin-left: 82%;
+}
+
+.footer-txt-2 h1{
+    font-size: 17.5px;
+
+}
+
+.footer-txt-2 img{
+    position: absolute;
+    height: 40px;
+    margin-left: -36%;
+    margin-top: 20%;
+}
+
+
+.footer-sections-3{
+    position: absolute;
+    margin-top: 7%;
+    margin-left: 82%;
+    font-family: 'Open Sans', sans-serif;
+    color: rgb(255, 255, 255);
+}
+
+.footer-sections-3 h2{
+    font-size: 13.5px;
+    
+}
+
+.line{
+    position: absolute;
+    height: 0.5px;
+    width: 95%;
+    background-color: rgb(149, 149, 149);
+    margin-left: 2.5%;
+    margin-top: 20%;
+}
+
+.last-footer{
+    width: 95%;
+    height: 50px;
+    background-color: rgba(0, 0, 0, 0);
+    margin-top: 22%;
+    color: rgb(172, 172, 172);
+    font-family: 'Open Sans', sans-serif;
+    margin-left: 3%;
+    font-size: 7px;
+}
+
+</style>
